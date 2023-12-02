c39b9f15162b30051b6ead4638bb2be149942af4
Benjamin Benais <benaisbenjamin@gmail.com>
2023-12-02 19:10:29 +0100

    day-2 part-2

--- src/2023/day-2/solution.ts
+++ src/2023/day-2/solution.ts
@@ -16,26 +16,63 @@ const limits = {
   blue: 14,
 } as const;
 
-function processLine(line: string): number {
-  const gameId = line.match(/\d+/)?.[0];
-
-  const cubesRegex = new RegExp(/(\d+\s\w+)/, 'g');
-  const cubes = line.match(cubesRegex) || [];
-
-  const isInvalid = cubes.some((cube) => {
-    const [n, color] = cube.split(' ') as [number, Color];
-    return Number(n) > limits[color];
-  });
-
-  return isInvalid ? 0 : Number(gameId);
+function getGameId(line: string) {
+  return line.match(/\d+/)?.[0];
 }
 
-function main() {
+function getCubes(line: string) {
+  const r = new RegExp(/(\d+\s\w+)/, 'g');
+  return line.match(r) || [];
+}
+
+function parseCube(cube: string): [number, Color] {
+  const [count, color] = cube.split(' ') as [number, Color];
+  return [Number(count), color];
+}
+
+function processPartOne(line: string): number {
+  const gameId = getGameId(line);
+  const cubes = getCubes(line);
+
+  const isGameImpossible = cubes.some((cube) => {
+    const [count, color] = parseCube(cube);
+    return Number(count) > limits[color];
+  });
+
+  return isGameImpossible ? 0 : Number(gameId);
+}
+
+function processPartTwo(line: string): number {
+  const cubes = getCubes(line);
+
+  let maxRed = 0;
+  let maxBlue = 0;
+  let maxGreen = 0;
+
+  cubes.forEach((cube) => {
+    const [count, color] = parseCube(cube);
+
+    if (color === 'red' && count > maxRed) {
+      maxRed = count;
+    } else if (color === 'green' && count > maxGreen) {
+      maxGreen = count;
+    } else if (color === 'blue' && count > maxBlue) {
+      maxBlue = count;
+    }
+  });
+
+  return maxRed * maxGreen * maxBlue;
+}
+
+function main(fn: (line: string) => number) {
   return puzzleInput
     .split('\n')
-    .map(processLine)
+    .map(fn)
     .reduce((acc, curr) => acc + curr, 0);
 }
 
-const partOneResult = main();
+const partOneResult = main(processPartOne);
 console.log('partOneResult: ', partOneResult);
+
+const partTwoResult = main(processPartTwo);
+console.log('partTwoResult: ', partTwoResult);
