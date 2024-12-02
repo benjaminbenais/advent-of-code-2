772a58eebdbd4b9f7692404238d59f0d30316510
Benjamin Benais <benaisbenjamin@gmail.com>
2024-12-02 22:14:21 +0100

    2024 day-1

---
+++ src/2024/day-1/solution.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from '../../utils/readFileSync';
+
+const puzzleInput = readFileSync('./puzzle-input.txt', import.meta.url).split(
+  '\n',
+);
+
+type List = number[];
+type Lists = [List, List];
+
+function getLists(rawInput: string[]): Lists {
+  const list1: List = [];
+  const list2: List = [];
+
+  rawInput.forEach((line) => {
+    const [left, right] = line.split('   ');
+    list1.push(+left);
+    list2.push(+right);
+  });
+
+  return [list1.sort(), list2.sort()];
+}
+
+function part1(lists: Lists): number {
+  const [list1, list2] = lists;
+
+  let result = 0;
+
+  list1.forEach((n, i) => {
+    result += Math.abs(n - list2[i]);
+  });
+
+  return result;
+}
+
+function part2(lists: Lists): number {
+  const [list1, list2] = lists;
+  let occurences = 0;
+  let result = 0;
+
+  list1.forEach((x) => {
+    list2.forEach((y) => {
+      if (x === y) {
+        occurences += 1;
+      }
+    });
+
+    result += x * occurences;
+    occurences = 0;
+  });
+
+  return result;
+}
+
+function main() {
+  const lists = getLists(puzzleInput);
+
+  console.log('Part 1 result: ', part1(lists));
+  console.log('Part 2 result: ', part2(lists));
+}
+
+main();
